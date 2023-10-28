2475b0cadce8c67f09afc5c95402e199252f50ec
Anton Damlin <anton.damlin@comsel.fi>
2023-10-28 12:38:53 +0300

    Assignment 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,17 +5,31 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0.0);
+  const [positive, setPositive] = useState(0.0);
 
+  const updateStats = (good, neutral, bad) => {
+    const newAll = good + neutral + bad;
+    const newAverage = (good * 1 + neutral * 0 + bad * -1) / newAll;
+    const newPositive = good / newAll;
+    setAll(newAll);
+    setAverage(newAverage);
+    setPositive(newPositive);
+  };
   const handleGoodClick = () => {
     const newGood = good + 1;
+    updateStats(newGood, neutral, bad);
     setGood(newGood);
   };
   const handleNeutralClick = () => {
     const newNeutral = neutral + 1;
+    updateStats(good, newNeutral, bad);
     setNeutral(newNeutral);
   };
   const handleBadClick = () => {
     const newBad = bad + 1;
+    updateStats(good, neutral, newBad);
     setBad(newBad);
   };
   return (
@@ -30,6 +44,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </div>
   );
 };
