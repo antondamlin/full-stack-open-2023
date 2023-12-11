acad7cc098a108dc77a4bd9462c9d28bf482fb46
Anton Damlin <anton.damlin@comsel.fi>
2023-12-11 11:14:40 +0200

    Assignment 3.7

--- part2/countrydata/src/services/countries.js
+++ part2/countrydata/src/services/countries.js
@@ -2,7 +2,6 @@ import axios from "axios";
 const api_key = import.meta.env.VITE_API_KEY
 const baseUrlCountry = "https://studies.cs.helsinki.fi/restcountries/api/";
 
-//http://api.weatherapi.com/v1/current.json?key=<YOUR_API_KEY>&q=London
 const getAll = () => {
   return axios.get(baseUrlCountry + "all");
 };
