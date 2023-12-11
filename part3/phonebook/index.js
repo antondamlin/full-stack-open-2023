6c975445e70a6204adf233f9ae1025447c43f77b
Anton Damlin <anton.damlin@comsel.fi>
2023-12-11 08:28:01 +0200

    Assignment 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -25,8 +25,18 @@ let persons = [
   },
 ];
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (response) => {
   response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((per) => per.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
 });
 
 app.get("/info", (request, response) => {
