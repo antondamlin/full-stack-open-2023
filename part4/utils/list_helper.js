00a5785974369ee9a23810fe2ee42cd0367f59a3
Anton Damlin <anton.damlin@comsel.fi>
2023-12-15 16:53:42 +0200

    Assignment 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,14 @@
 const dummy = (blogs) => {
-  return 1
-}
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  let likes = 0;
+  blogs.forEach((post) => (likes += post.likes));
+  return likes;
+};
 
 module.exports = {
-  dummy
-}
+  dummy,
+  totalLikes,
+};
