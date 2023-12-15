a76e0585f937811b25b46199774c61be67a5acc0
Anton Damlin <anton.damlin@comsel.fi>
2023-12-15 17:36:40 +0200

    Assignment 4.6

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,25 +1,43 @@
 const dummy = (blogs) => {
-  return 1;
-};
+  return 1
+}
 
 const totalLikes = (blogs) => {
-  let likes = 0;
-  blogs.forEach((post) => (likes += post.likes));
-  return likes;
-};
+  let likes = 0
+  blogs.forEach((post) => (likes += post.likes))
+  return likes
+}
 
 const favoriteBlog = (blogs) => {
-  const maxlikes = Math.max(...blogs.map((post) => post.likes));
-  const returnObject = blogs.filter((post) => post.likes === maxlikes);
+  const maxlikes = Math.max(...blogs.map((post) => post.likes))
+  const returnObject = blogs.filter((post) => post.likes === maxlikes)
   if (returnObject.length > 0) {
-    return returnObject[0];
+    return returnObject[0]
   } else {
-    return {};
+    return {}
   }
-};
+}
+
+const mostBlogs = (blogs) => {
+  const result = blogs.reduce((first, second) => {
+    let author = first.find((blog) => {
+      return blog.author === second.author
+    })
+    if (!author) {
+      return first.concat({ author: second.author, blogs: 1 })
+    }
+    author.blogs += 1
+    return first
+  }, [])
+
+  return result.reduce((first, second) =>
+    first.blogs > second.blogs ? first : second
+  )
+}
 
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
-};
+  mostBlogs,
+}
