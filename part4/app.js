a0201e171dc2fa075126226fcae2981f4b3b1870
Anton Damlin <anton.damlin@comsel.fi>
2023-12-19 07:25:19 +0200

    Assignment 4.15

--- part4/app.js
+++ part4/app.js
@@ -1,33 +1,36 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogRouter = require('./controllers/blogList')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogRouter = require("./controllers/blogList");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+const usersRouter = require("./controllers/users");
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info("connecting to", config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
+    logger.error("error connecting to MongoDB:", error.message);
+  });
 
-app.use(cors())
-app.use(express.static('dist'))
-app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.use('/api/blogs', blogRouter)
+app.use("/api/blogs", blogRouter);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use("/api/users", usersRouter);
 
-module.exports = app
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
