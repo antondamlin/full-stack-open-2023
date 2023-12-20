f2bbc877bd8fc0793c0d718112d7f858d2bccc90
Anton Damlin <anton.damlin@comsel.fi>
2023-12-20 13:48:51 +0200

    Assignment 5.2

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -5,12 +5,21 @@ import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('') 
-  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  useEffect(() => {
+    const userLocal = window.localStorage.getItem("loggedBlogUser");
+    if (userLocal) {
+      const user = JSON.parse(userLocal);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
   }, []);
 
   const handleLogin = async (event) => {
@@ -21,6 +30,7 @@ const App = () => {
         username,
         password,
       });
+      window.localStorage.setItem("loggedBlogUser", JSON.stringify(user));
       setUser(user);
       setUsername("");
       setPassword("");
@@ -32,6 +42,11 @@ const App = () => {
     }
   };
 
+  const handleLogout = (event) => {
+    event.preventDefault();
+    window.localStorage.removeItem("loggedBlogUser", JSON.stringify(user));
+    setUser(null);
+  };
   if (user === null) {
     return (
       <div>
@@ -64,7 +79,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <div style={{ display: "flex" }}>
+        <p>{user.name} logged in</p>{" "}
+        <button onClick={handleLogout}>logout</button>
+      </div>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
