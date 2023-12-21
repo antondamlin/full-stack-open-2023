323097a0428a061629cfce8eca762de92ca16a33
Anton Damlin <anton.damlin@comsel.fi>
2023-12-21 19:29:15 +0200

    Assignment 5.8

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+  const [buttonText, setButtonText] = useState("view");
+  const show = { display: visible ? "" : "none" };
+
+  const handleViewChange = () => {
+    const buttonText = !visible ? "hide" : "view";
+    setVisible(!visible);
+    setButtonText(buttonText);
+  };
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={handleViewChange}>{buttonText}</button>
+      </div>
+      <div style={show}>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes}
+          <button>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
