49a0888f496716b5103b25e134209681566a87fe
Anton Damlin <anton.damlin@comsel.fi>
2023-12-22 14:06:39 +0200

    Assignment 5.15

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { render, screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
@@ -48,4 +48,26 @@ test('URL and Likes are shown when view button is clicked', async () => {
   await user.click(button)
   const expandedBlogInfo = cont.querySelector('.blogInfoExpanded')
   expect(expandedBlogInfo).not.toHaveStyle('display: none')
+})
+
+test('Props component called twice when like button is clicked twice', async () => {
+  const blog = {
+    id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    user: { name: 'User_1' },
+  }
+
+  const user = userEvent.setup()
+  let mockLikeBlog = jest.fn()
+  let mockDeleteBlog = jest.fn()
+
+  const cont = render(
+    <Blog blog={blog} addLike={mockLikeBlog} deleteBlog={mockDeleteBlog} />
+  ).container
+  const button = screen.getByText('like')
+  await user.click(button)
+  expect(mockLikeBlog.mock.calls).toHaveLength(1)
 })
