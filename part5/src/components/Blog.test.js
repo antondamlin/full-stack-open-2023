39718a04e01726e86a87bb1e6df57b40f884e451
Anton Damlin <anton.damlin@comsel.fi>
2023-12-22 13:52:57 +0200

    Assignment 5.14

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,28 +1,51 @@
-import React from "react";
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import Blog from "./Blog";
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Blog from './Blog'
 
-test("Displaying title and author but not likes and url", () => {
+test('Displaying title and author but not likes and url', () => {
   const blog = {
-    id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
+    id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
     likes: 7,
-    user: { name: "User_1" },
-  };
+    user: { name: 'User_1' },
+  }
 
-  let mockLikeBlog = jest.fn();
-  let mockDeleteBlog = jest.fn();
+  let mockLikeBlog = jest.fn()
+  let mockDeleteBlog = jest.fn()
 
   const cont = render(
     <Blog blog={blog} addLike={mockLikeBlog} deleteBlog={mockDeleteBlog} />
-  ).container;
-  screen.debug();
-  const titleAndAuthor = blog.title + " " + blog.author;
-  const titleElement = cont.querySelector('.blogInfo');
-  const expandedBlogInfo = cont.querySelector('.blogInfoExpanded');
-  expect(titleElement).toHaveTextContent(titleAndAuthor);
+  ).container
+  const titleAndAuthor = blog.title + ' ' + blog.author
+  const titleElement = cont.querySelector('.blogInfo')
+  const expandedBlogInfo = cont.querySelector('.blogInfoExpanded')
+  expect(titleElement).toHaveTextContent(titleAndAuthor)
   expect(expandedBlogInfo).toHaveStyle('display: none')
-});
+})
+
+test('URL and Likes are shown when view button is clicked', async () => {
+  const blog = {
+    id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    user: { name: 'User_1' },
+  }
+
+  const user = userEvent.setup()
+  let mockLikeBlog = jest.fn()
+  let mockDeleteBlog = jest.fn()
+
+  const cont = render(
+    <Blog blog={blog} addLike={mockLikeBlog} deleteBlog={mockDeleteBlog} />
+  ).container
+  const button = screen.getByText('view')
+  await user.click(button)
+  const expandedBlogInfo = cont.querySelector('.blogInfoExpanded')
+  expect(expandedBlogInfo).not.toHaveStyle('display: none')
+})
