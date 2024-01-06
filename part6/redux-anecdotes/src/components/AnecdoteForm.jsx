76c37b240e36e64296f3b5dc153d5baeae1f38d7
Anton Damlin <anton.damlin@comsel.fi>
2024-01-06 13:59:32 +0200

    Assignment 6.16 & 6.17

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,5 +1,4 @@
-import anecdoteService from '../services/anecdotes';
-import { addAnecdote } from '../reducers/anecdoteReducer';
+import { createAnecdote } from '../reducers/anecdoteReducer';
 import { useDispatch } from 'react-redux';
 import { setTimeoutNotification } from '../reducers/notificationReducer';
 
@@ -8,10 +7,8 @@ const AnecdoteForm = () => {
   const handleAdd = async (event) => {
     event.preventDefault();
     const val = event.target.anecdote.value;
+    dispatch(createAnecdote(val));
     event.target.anecdote.value = '';
-    const anecData = await anecdoteService.createNew(val);
-    console.log(anecData);
-    dispatch(addAnecdote(anecData));
     const message = 'you added anecdote a new anecdote: \'' + val + '\'';
     dispatch(setTimeoutNotification(message, 5));
   };
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import anecdoteServices from '../services/anecdotes';
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -28,6 +29,21 @@ const anecdoteSlice = createSlice({
 
 export const { addAnecdote, voteAnecdote, appendAnecdote, setAnecdotes } =
   anecdoteSlice.actions;
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteServices.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
+
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const anecdote = await anecdoteServices.createNew(content);
+    dispatch(appendAnecdote(anecdote));
+  };
+};
+
 export default anecdoteSlice.reducer;
 
 /*const anecdoteReducer = (state = initialState, action) => {
