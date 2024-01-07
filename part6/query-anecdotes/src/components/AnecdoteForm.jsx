66ddcca65110a0bc4bbb4d86c2f18afea16d3c5a
Anton Damlin <anton.damlin@comsel.fi>
2024-01-07 12:43:05 +0200

    Assignment 6.21

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,5 +1,5 @@
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { getAll, createAnecdote } from "../requests";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createAnecdote } from "../requests";
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient();
