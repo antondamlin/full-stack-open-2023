d86f8f0e6c2c45dd5ccb6291c7ee678b47b1cdde
Anton Damlin <anton.damlin@comsel.fi>
2024-01-07 12:42:40 +0200

    Assignment 6.21

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,24 +1,40 @@
-import { useQuery, useMutation } from '@tanstack/react-query'
-import { getAll, createAnecdote } from '../requests'
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAll, createAnecdote } from "../requests";
 
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient();
+
+  const newAnecMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+    },
+  });
+
+  const getId = () => (100000 * Math.random()).toFixed(0);
 
   const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    event.preventDefault();
+    const anecContent = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    console.log("new anecdote");
+    const anecObject = {
+      content: anecContent,
+      votes: 0,
+      id: getId(),
+    };
+    newAnecMutation.mutate(anecObject);
+  };
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -1,16 +1,9 @@
 import axios from "axios";
 
-const getId = () => (100000 * Math.random()).toFixed(0);
-
 export const getAll = () =>
   axios.get("http://localhost:3001/anecdotes").then((res) => res.data);
 
-export const createAnecdote = (anecContent) => {
-  const anecObject = {
-    content: anecContent,
-    votes: 0,
-    id: getId(),
-  };
+export const createAnecdote = (anecObject) => {
   axios
     .post("http://localhost:3001/anecdotes", anecObject)
     .then((res) => res.data);
