b2f66346e60b8be7f0d1187538629bd164afe472
Anton Damlin <anton.damlin@comsel.fi>
2024-01-14 08:40:38 +0200

    Assignment 7.4

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useField } from "./hooks/index";
 import { Routes, Route, Link, useMatch, useNavigate } from "react-router-dom";
 
 const Menu = () => {
@@ -78,17 +79,17 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField('text')
+  const author = useField('text')
+  const url = useField('text')
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: url.value,
       votes: 0,
     });
     navigate("/");
@@ -102,24 +103,27 @@ const CreateNew = (props) => {
           content
           <input
             name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            type={content.type}
+            value={content.value}
+            onChange={content.onChange}
           />
         </div>
         <div>
           author
           <input
             name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
+            type={author.type}
+            value={author.value}
+            onChange={author.onChange}
           />
         </div>
         <div>
           url for more info
           <input
             name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
+            type={url.type}
+            value={url.value}
+            onChange={url.onChange}
           />
         </div>
         <button>create</button>
